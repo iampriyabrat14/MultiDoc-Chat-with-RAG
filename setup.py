735602269819a52d191476d558cb7e3b59f148da
iampriyabrat14 <datasc521@gmail.com>
2025-07-20 02:43:56 +0530

    setup the project

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MultiDoc-Chat-with-RAG",   
+    version="0.1.0",
+    author="Priyabrat Dalbehera",
+    description="A RAG-powered GenAI system to chat, analyze, and compare multiple documents intelligently.",
+    packages=find_packages(),
+    # install_requires=[  
+    #     # "langchain", "openai", "faiss-cpu", "pydantic",
+    # ],
+)
+
+
+
